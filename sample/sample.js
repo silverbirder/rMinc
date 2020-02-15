582ac6258f17873c4ffe628fc1d9e7020e5de786
shibamoto <silverbirder@gmail.com>
2020-02-15 17:12:21 +0900

    Fixed Shokutakubin mail process

--- sample/sample.js
+++ sample/sample.js
@@ -1,6 +1,11 @@
 function myFunction() {
     const user = new User();
-    user.setMailRules([new TohoCinemasMailRule(), new AmazonMailRule(), new ShokutakubinMailRule()]);
+    const after = new Date('2019-11-01');
+    const before = new Date('2019-12-01');
+    const mail1 = new TohoCinemasMailRule(before, after);
+    const mail2 = new AmazonMailRule(before, after);
+    const mail3 = new ShokutakubinMailRule(before, after);
+    user.setMailRules([mail1, mail2, mail3]);
     user.fetchMails();
     user.doRMinc();
 }
