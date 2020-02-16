ea024e3a3f4dc7740b13f469efbc854c3ecbad4d
shibamoto <silverbirder@gmail.com>
2020-02-16 18:59:21 +0900

    Publish the MailRule Class

--- sample/sample.js
+++ sample/sample.js
@@ -5,7 +5,28 @@ function myFunction() {
     const mail1 = new RMinc.TohoCinemasMailRule(before, after);
     const mail2 = new RMinc.AmazonMailRule(before, after);
     const mail3 = new RMinc.ShokutakubinMailRule(before, after);
-    user.setMailRules([mail1, mail2, mail3]);
+    const mail4 = new YourFavoriteService(before, after);
+    user.setMailRules([mail1, mail2, mail3, mail4]);
     user.fetchMails();
     user.doRMinc();
+}
+
+class YourFavoriteService extends RMinc.MailRule {
+    constructor() {
+        super();
+        this.name = 'name';
+        this.filteringKeyword = 'from:(shipment@service.com)';
+    }
+
+    extractLocation(body) {
+        return 'location';
+    }
+
+    extractDateRange(body) {
+        return {start: new Date(), end: new Date()}
+    }
+
+    extractTitle(body) {
+        return 'title';
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,25 @@ import UserImpl from "./user/userImpl";
 import AmazonMailRule from "./user/mailRule/amazonMailRule";
 import TohoCinemasMailRule from "./user/mailRule/tohoCinemasMailRule";
 import ShokutakubinMailRule from "./user/mailRule/shokutakubinMailRule";
+import MailRule from "./user/mailRule/mailRule";
 
 declare const global: {
     User: any
+    MailRule: any
     AmazonMailRule: any
     TohoCinemasMailRule: any
     ShokutakubinMailRule: any
 };
 
 global.User = UserImpl;
+global.MailRule = MailRule;
 global.AmazonMailRule = AmazonMailRule;
 global.TohoCinemasMailRule = TohoCinemasMailRule;
 global.ShokutakubinMailRule = ShokutakubinMailRule;
 
 export {
     UserImpl as User,
+    MailRule,
     AmazonMailRule,
     TohoCinemasMailRule,
     ShokutakubinMailRule,
